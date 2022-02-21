fe75d1b92383d5473e4d7c28a2e462d900d1cd4d
kis8ya <kis8P@yandex.ru>
2022-02-21 23:53:31 +0300

    initial commit

---
+++ src/main/java/com/github/kis8ya/recipe/RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.github.kis8ya.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kis8ya/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kis8ya.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
